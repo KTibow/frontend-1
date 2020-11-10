b5aff288d238a6731a2eb24e2b0a33b12ef06ac5
Kendell R <KTibow@users.noreply.github.com>
2020-11-09 17:57:08 -0800

    Revert "Support system health streaming (#7593)"
    This reverts commit 721a71b43e990e8f9f45774262e01b4f6d31a56f.

--- src/panels/config/info/system-health-card.ts
+++ src/panels/config/info/system-health-card.ts
@@ -1,5 +1,3 @@
-import "@material/mwc-button/mwc-button";
-import "@material/mwc-icon-button";
 import "../../../components/ha-circular-progress";
 import { mdiContentCopy } from "@mdi/js";
 import {
@@ -17,13 +15,10 @@ import "@polymer/paper-tooltip/paper-tooltip";
 import type { PaperTooltipElement } from "@polymer/paper-tooltip/paper-tooltip";
 import { domainToName } from "../../../data/integration";
 import {
-  subscribeSystemHealthInfo,
+  fetchSystemHealthInfo,
   SystemHealthInfo,
-  SystemCheckValueObject,
 } from "../../../data/system_health";
 import { HomeAssistant } from "../../../types";
-import { formatDateTime } from "../../../common/datetime/format_date_time";
-import { copyToClipboard } from "../../../common/util/copy-clipboard";
 
 const sortKeys = (a: string, b: string) => {
   if (a === "homeassistant") {
@@ -65,74 +60,19 @@ class SystemHealthCard extends LitElement {
     } else {
       const domains = Object.keys(this._info).sort(sortKeys);
       for (const domain of domains) {
-        const domainInfo = this._info[domain];
         const keys: TemplateResult[] = [];
 
-        for (const key of Object.keys(domainInfo.info)) {
-          let value: unknown;
-
-          if (typeof domainInfo.info[key] === "object") {
-            const info = domainInfo.info[key] as SystemCheckValueObject;
-
-            if (info.type === "pending") {
-              value = html`
-                <ha-circular-progress active size="tiny"></ha-circular-progress>
-              `;
-            } else if (info.type === "failed") {
-              value = html`
-                <span class="error">${info.error}</span>${!info.more_info
-                  ? ""
-                  : html`
-                      –
-                      <a
-                        href=${info.more_info}
-                        target="_blank"
-                        rel="noreferrer noopener"
-                      >
-                        ${this.hass.localize(
-                          "ui.panel.config.info.system_health.more_info"
-                        )}
-                      </a>
-                    `}
-              `;
-            } else if (info.type === "date") {
-              value = formatDateTime(new Date(info.value), this.hass.language);
-            }
-          } else {
-            value = domainInfo.info[key];
-          }
-
+        for (const key of Object.keys(this._info[domain]).sort()) {
           keys.push(html`
             <tr>
-              <td>
-                ${this.hass.localize(
-                  `ui.panel.config.info.system_health.checks.${domain}.${key}`
-                ) || key}
-              </td>
-              <td>${value}</td>
+              <td>${key}</td>
+              <td>${this._info[domain][key]}</td>
             </tr>
           `);
         }
         if (domain !== "homeassistant") {
           sections.push(
-            html`
-              <div class="card-header">
-                <h3>
-                  ${domainToName(this.hass.localize, domain)}
-                </h3>
-                ${!domainInfo.manage_url
-                  ? ""
-                  : html`
-                      <a class="manage" href=${domainInfo.manage_url}>
-                        <mwc-button>
-                          ${this.hass.localize(
-                            "ui.panel.config.info.system_health.manage"
-                          )}
-                        </mwc-button>
-                      </a>
-                    `}
-              </div>
-            `
+            html`<h3>${domainToName(this.hass.localize, domain)}</h3>`
           );
         }
         sections.push(html`
@@ -169,63 +109,45 @@ class SystemHealthCard extends LitElement {
 
   protected firstUpdated(changedProps) {
     super.firstUpdated(changedProps);
+    this._fetchInfo();
+  }
 
-    if (!this.hass!.config.components.includes("system_health")) {
+  private async _fetchInfo() {
+    try {
+      if (!this.hass!.config.components.includes("system_health")) {
+        throw new Error();
+      }
+      this._info = await fetchSystemHealthInfo(this.hass!);
+    } catch (err) {
       this._info = {
         system_health: {
-          info: {
-            error: this.hass.localize(
-              "ui.panel.config.info.system_health_error"
-            ),
-          },
+          error: this.hass.localize("ui.panel.config.info.system_health_error"),
         },
       };
-      return;
     }
-
-    subscribeSystemHealthInfo(this.hass!, (info) => {
-      this._info = info;
-    });
   }
 
   private _copyInfo(): void {
-    let haContent: string | undefined;
-    const domainParts: string[] = [];
+    const copyElement = this.shadowRoot?.querySelector(
+      ".card-content"
+    ) as HTMLElement;
 
-    for (const domain of Object.keys(this._info!).sort(sortKeys)) {
-      const domainInfo = this._info![domain];
-      const parts = [`${domainToName(this.hass.localize, domain)}\n`];
+    // Add temporary heading (fixed in EN since usually executed to provide support data)
+    const tempTitle = document.createElement("h3");
+    tempTitle.innerText = "System Health";
+    copyElement.insertBefore(tempTitle, copyElement.firstElementChild);
 
-      for (const key of Object.keys(domainInfo.info)) {
-        let value: unknown;
+    const selection = window.getSelection()!;
+    selection.removeAllRanges();
+    const range = document.createRange();
+    range.selectNodeContents(copyElement);
+    selection.addRange(range);
 
-        if (typeof domainInfo.info[key] === "object") {
-          const info = domainInfo.info[key] as SystemCheckValueObject;
+    document.execCommand("copy");
+    window.getSelection()!.removeAllRanges();
 
-          if (info.type === "pending") {
-            value = "pending";
-          } else if (info.type === "failed") {
-            value = `failed to load: ${info.error}`;
-          } else if (info.type === "date") {
-            value = formatDateTime(new Date(info.value), this.hass.language);
-          }
-        } else {
-          value = domainInfo.info[key];
-        }
-
-        parts.push(`${key}: ${value}`);
-      }
-
-      if (domain === "homeassistant") {
-        haContent = parts.join("\n");
-      } else {
-        domainParts.push(parts.join("\n"));
-      }
-    }
-
-    copyToClipboard(
-      `System Health\n\n${haContent}\n\n${domainParts.join("\n\n")}`
-    );
+    // Remove temporary heading again
+    copyElement.removeChild(tempTitle);
 
     this._toolTip!.show();
     setTimeout(() => this._toolTip?.hide(), 3000);
@@ -238,7 +160,7 @@ class SystemHealthCard extends LitElement {
       }
 
       td:first-child {
-        width: 45%;
+        width: 33%;
       }
 
       .loading-container {
@@ -250,19 +172,6 @@ class SystemHealthCard extends LitElement {
       .card-header {
         justify-content: space-between;
         display: flex;
-        align-items: center;
-      }
-
-      .error {
-        color: var(--error-color);
-      }
-
-      a {
-        color: var(--primary-color);
-      }
-
-      a.manage {
-        text-decoration: none;
       }
     `;
   }
