7562a93ee3f9113b1977d5b903bd5b548c94dc66
Kendell R <KTibow@users.noreply.github.com>
2020-11-09 17:57:08 -0800

    Revert "Add ingress session validation (#7610)"
    This reverts commit cdbc31ce234f39c2183aabe9e6850e929956b76e.

--- hassio/src/ingress-view/hassio-ingress-view.ts
+++ hassio/src/ingress-view/hassio-ingress-view.ts
@@ -13,10 +13,7 @@ import {
   fetchHassioAddonInfo,
   HassioAddonDetails,
 } from "../../../src/data/hassio/addon";
-import {
-  createHassioSession,
-  validateHassioSession,
-} from "../../../src/data/hassio/ingress";
+import { createHassioSession } from "../../../src/data/hassio/supervisor";
 import "../../../src/layouts/hass-loading-screen";
 import "../../../src/layouts/hass-subpage";
 import { HomeAssistant, Route } from "../../../src/types";
@@ -37,17 +34,6 @@ class HassioIngressView extends LitElement {
 
   @property({ type: Boolean })
   public narrow = false;
-
-  private _sessionKeepAlive?: number;
-
-  public disconnectedCallback() {
-    super.disconnectedCallback();
-
-    if (this._sessionKeepAlive) {
-      clearInterval(this._sessionKeepAlive);
-      this._sessionKeepAlive = undefined;
-    }
-  }
 
   protected render(): TemplateResult {
     if (!this._addon) {
@@ -97,7 +83,10 @@ class HassioIngressView extends LitElement {
   }
 
   private async _fetchData(addonSlug: string) {
-    const createSessionPromise = createHassioSession(this.hass);
+    const createSessionPromise = createHassioSession(this.hass).then(
+      () => true,
+      () => false
+    );
 
     let addon;
 
@@ -130,11 +119,7 @@ class HassioIngressView extends LitElement {
       return;
     }
 
-    let session;
-
-    try {
-      session = await createSessionPromise;
-    } catch (err) {
+    if (!(await createSessionPromise)) {
       await showAlertDialog(this, {
         text: "Unable to create an Ingress session",
         title: addon.name,
@@ -142,17 +127,6 @@ class HassioIngressView extends LitElement {
       history.back();
       return;
     }
-
-    if (this._sessionKeepAlive) {
-      clearInterval(this._sessionKeepAlive);
-    }
-    this._sessionKeepAlive = window.setInterval(async () => {
-      try {
-        await validateHassioSession(this.hass, session);
-      } catch (err) {
-        session = await createHassioSession(this.hass);
-      }
-    }, 60000);
 
     this._addon = addon;
   }
